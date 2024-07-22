2565f262e9ce2b797df4186b310f38ea12612bf7
MoonKyoungHun <lsf8355@gmail.com>
2024-07-22 16:07:31 +0900

    Charater
    Charater basic

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP1
+{
+    internal class Program
+    {
+        class Chracter
+        {
+            //객체가 가지는 데이터(필드)
+            public int level;          //레벨
+            public int hp;             //체력
+            public float speed;        //이동속도
+            public float damage;       //공격력
+
+            //객체가 가지는 동작(메서드)
+            void Go()   //전진
+            {
+            }
+
+            void Back()     //후진
+            {
+            }
+
+            void LeftTurn()     //좌회전(90도)
+            {
+            }
+
+            void RightTurn()        //우회전(90도)
+            {
+            }
+
+            void Attack()       //공격
+            {
+            }
+
+            private void Hit()      //피격
+            {
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
